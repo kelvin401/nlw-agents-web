e4075c96aa928d9db7c53cfe2dca4399343bb731
kelvincarvalho <kelvin.nirvana@gmail.com>
2025-07-11 22:08:37 -0300

    feat: add react-router-dom and react-query

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
           <Route element={<CreateRoom />} path="/" />
-          <Route element={<Room />} path="/rooms" />
+          <Route element={<Room />} path="/rooms/:roomId" />
         </Routes>
       </BrowserRouter>
-    </div>
+    </QueryClientProvider>
   )
 }
--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,21 +1,38 @@
+import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
 
+type GetRoomsAPIResponse = Array<{
+  id: string
+  name: string
+}>
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms')
+      const result: GetRoomsAPIResponse = await response.json()
+
+      return result
+    },
+  })
+
   return (
     <div>
-      <div>Create Room</div>
+      {isLoading && <div>Carregando...</div>}
+      <div className="flex flex-col gap-1">
+        {data?.map((room) => {
+          return (
+            <Link key={room.id} to={`/rooms/${room.id}`}>
+              {room.name}
+            </Link>
+          )
+        })}
+      </div>
 
       <div className="mt-10 flex flex-col gap-2">
-        <Link className="underline" to="/rooms/lets-code-together">
+        <Link className="underline" to="/room">
           Acessar sala
-        </Link>
-
-        <Link
-          className="underline"
-          target="_blank"
-          to="https://youtu.be/gBTiQNF7ZXI?t=4903"
-        >
-          Acessar aula do momento atual
         </Link>
       </div>
     </div>
