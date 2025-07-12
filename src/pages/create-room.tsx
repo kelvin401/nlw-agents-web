8c0880fe2c54cc654564bf6eca2d7c50330f5b1f
kelvincarvalho <kelvin.nirvana@gmail.com>
2025-07-12 12:15:52 -0300

    feat: add form to create rooms

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,39 +1,17 @@
-import { useQuery } from '@tanstack/react-query'
-import { Link } from 'react-router-dom'
-
-type GetRoomsAPIResponse = Array<{
-  id: string
-  name: string
-}>
+import { CreateRoomForm } from '@/components/create-room-form'
+import { RoomList } from '@/components/room-list'
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/rooms')
-      const result: GetRoomsAPIResponse = await response.json()
-
-      return result
-    },
-  })
-
   return (
-    <div>
-      {isLoading && <div>Carregando...</div>}
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => {
-          return (
-            <Link key={room.id} to={`/rooms/${room.id}`}>
-              {room.name}
-            </Link>
-          )
-        })}
-      </div>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomForm />
 
-      <div className="mt-10 flex flex-col gap-2">
-        <Link className="underline" to="/room">
-          Acessar sala
-        </Link>
+          <RoomList />
+
+          <a href="https://youtu.be/17CO2twuLOM?t=3701">continuar aula</a>
+        </div>
       </div>
     </div>
   )
